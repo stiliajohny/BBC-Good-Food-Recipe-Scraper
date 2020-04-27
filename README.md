b8050867c1986b563db63dbdf3477bf5c44af94f
John Stilia <stilia.johny+internal.gitlab.perosnal@gmail.com>
2020-04-27 01:31:19 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Recipe Scraper 
+  
+  
+## This app is made in order to extract a list of ingrediants from a url given based on bbcgoodfoor.com
+
+### How To
+
+* source ./bin/activate 
+* python ./recipe_scraper.py https://bbcgoodfood.com/recipe/SOME_RECIPE
+
---
+++ recipe_scraper.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+if len (sys.argv) != 2 :
+    print("Usage: python recipe_scraper.py URL")
+    sys.exit (1)
+
+headers = {'User-Agent': 'Mozilla/5.0'}
+#url = "https://www.bbcgoodfood.com/recipes/orzo-tomato-soup" # sys.argv[1]
+url = sys.argv[1]
+
+requested_url = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(requested_url.text, 'html.parser')
+
+results = soup.find(id='recipe-ingredients')
+print(soup.title.string)
+print("\n")
+for tag in results.ul.find_all("li", recursive=True):
+    print(tag.get('content'))
